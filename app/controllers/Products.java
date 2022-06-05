6bc24a095d65c212704483aaa194f219e0390e99
Leonardo Fabio Mercado Benítez <lfmercadobe@unal.edu.co>
2022-06-05 01:31:04 -0500

    Se genera la vista details, se vincula con el formulario de la clase Products y se agrega el messages API

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,18 +1,36 @@
 package controllers;
 
 import models.Product;
+import play.i18n.Lang;
+import play.i18n.MessagesApi;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
+import javax.inject.Inject;
 import views.html.products.*;
 
+import java.util.ArrayList;
+
 public class Products extends Controller {
+    private final Form<Product> productoForm;
+    private MessagesApi messagesApi;
+    public final static ArrayList<Lang> idioma = new ArrayList<Lang>(){{
+        add(Lang.forCode("es"));
+    }};
+
+    @Inject
+    public Products(FormFactory formFactory, MessagesApi messagesApi) {
+        this.productoForm = formFactory.form(Product.class);
+        this.messagesApi = messagesApi;
+    }
 
     public Result list(){
-        return ok(list.render(Product.findAll()));
+        return ok(list.render("Produtcs Catalogue",Product.findAll()));
     }
 
     public Result newProduct(){
-        return ok("llegamos a crear un nuevo producto");
+        return ok(details.render("Detalle del producto",productoForm, messagesApi.preferred(idioma)));
     }
 
     public Result details(String ean){
